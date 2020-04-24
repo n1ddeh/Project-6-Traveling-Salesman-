353906ab0ef2bdb1584e4bab52016699f9ac3a47
Mark Minkoff <markminkoff00@gmail.com>
2020-04-23 21:03:25 -0700

    Created first draft of templated header files for AdjacencyList and Node

--- main/main/AdjacencyList.h
+++ main/main/AdjacencyList.h
@@ -1,11 +1,36 @@
 #ifndef ADJACENCY_LIST_
 #define ADJACENCY_LIST_
 #include <iostream>
+#include "Node.h"
 
-class adjacency_list
+template<class T>
+class AdjacencyList
 {
 private:
+	std::unique_ptr<Node<T>> head_ptr_;
+	int node_count_;
+	bool seen_;
+public:
+	// --- Constructors --- ///
+	AdjacencyList();														// Default CTOR: Creates empty list with no nodes or data.							
+	AdjacencyList(const T some_data);										// Parameterized Constructor 
+	AdjacencyList(const AdjacencyList<T>& some_list);						// Copy Constructor
+	AdjacencyList<T>& operator=(const AdjacencyList<T>& some_list);			// Assignment Constructor
+	AdjacencyList(AdjacencyList<T>&& some_list);							// Move Constructors
+	AdjacencyList<T>& operator=(AdjacencyList<T>&& r_list);					// Move Assignment Constructor
+	~AdjacencyList();														// Destructor
 
+	// --- Helper Functions --- ///
+	bool isEmpty() const;
+	bool isFull() const;
+	bool isSeen() const;
+	int nodeCount() const;
+	bool checkExistance() const;
+	std::unique_ptr<Node<T>> getHeadPtr();
 
+	/// --- Data Functions --- ///
+	void insert(T some_data);
+	void remove(T some_data);
+	void clear();
 };
 #endif // ADJACENCY_LIST_
--- main/main/Node.h
+++ main/main/Node.h
@@ -4,12 +4,25 @@
 #include <memory>
 
 template <class T>
-class node
+class Node
 {
 private:
-
-	
+	std::unique_ptr<Node<T>> next_;
+	T data_;
 public:
+	// --- Constructors --- ///
+	Node();
+	Node(const T some_data);
+	Node(const T some_data, std::unique_ptr<Node<T>> next_node);
+	Node<T>& operator=(const Node<T>& r_node);
+	Node(Node<T>&& some_node);
+	Node<T>& operator=(Node<T>&& r_node);
+	~Node();
+
+	// --- Helper Functions --- ///
+	T getData();
+	std::unique_ptr<Node<T>> getNextPtr();
+	void setNextPtr(std::unique_ptr<Node<T>> a_node);
 };
 
 #endif //NODE_H_
