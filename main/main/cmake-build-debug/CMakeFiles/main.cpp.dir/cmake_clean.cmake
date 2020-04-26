d60e8111709aa48e4ab76e64e140e29eae913972
Ben Pearson <bpearson@nevada.unr.edu>
2020-04-25 20:36:38 -0700

    Added CMake files for CLion use

---
+++ main/main/cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/AdjacencyList.cpp.o"
+  "CMakeFiles/main.cpp.dir/Driver.cpp.o"
+  "main.cpp"
+  "main.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
