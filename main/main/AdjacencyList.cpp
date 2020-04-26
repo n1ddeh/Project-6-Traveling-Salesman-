4bfaa4037ca1da0d7d7e0f2b026b8054e86ab684
Mark Minkoff <markminkoff00@gmail.com>
2020-04-25 20:03:11 -0700

    Added PrintList() function in the AdjacencyList and added test cases in main()

---
+++ main/main/AdjacencyList.cpp
@@ -0,0 +1,52 @@
+#include "AdjacencyList.h"
+
+AdjacencyList::AdjacencyList() {
+	city_name_ = "Unknown";
+	seen_ = false;
+}
+
+AdjacencyList::AdjacencyList(std::string& city, std::vector<std::tuple<std::string, int>>& connections) {
+	city_name_ = city;
+	city_connections_ = connections;
+	seen_ = false;
+}
+
+AdjacencyList::~AdjacencyList() = default;
+
+bool AdjacencyList::IsSeen() const {
+	return seen_;
+}
+
+int AdjacencyList::ConnectionCount() const {
+	return static_cast<int>(city_connections_.size());
+}
+
+std::string AdjacencyList::GetCity() const {
+	return city_name_;
+}
+
+void AdjacencyList::PrintList() {
+	if (city_name_ == "Unknown" && city_connections_.empty())
+	{
+		std::cout << "Unknown city has no connections." << std::endl;
+		return;
+	}
+	if(city_name_ != "Unknown" && !city_connections_.empty())
+	{
+		std::cout << city_name_ << std::endl;
+		return;
+	}
+
+	std::vector<std::tuple<std::string, int>>::iterator it = city_connections_.begin();
+	std::cout << city_name_ << " -> ";
+	while (it != city_connections_.end())
+	{
+		std::cout << std::get<std::string>(*it);
+		++it;
+		if (it != city_connections_.end())
+		{
+			std::cout << " -> ";
+		}
+	}
+	
+}
--- main/main/Driver.cpp
+++ main/main/Driver.cpp
@@ -1,58 +1,85 @@
 #include <string>
 #include <vector>
 #include <tuple>
+#include <iostream>
 
 inline std::vector<std::tuple<std::string, int>> RenoConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Reno.
 {
-	std::tuple<std::string, int> San_Francisco = std::make_tuple ("San Francisco", 218);
-	std::tuple<std::string, int> Las_Vegas = std::make_tuple ("Las Vegas", 439);
-	std::tuple<std::string, int> Salt_Lake = std::make_tuple ("Salt Lake City", 518);
-	connections.push_back(San_Francisco);
-	connections.push_back(Las_Vegas);
-	connections.push_back(Salt_Lake);
+	const std::tuple<std::string, int> san_francisco = std::make_tuple ("San Francisco", 218);
+	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 439);
+	const std::tuple<std::string, int> salt_lake = std::make_tuple ("Salt Lake City", 518);
+	connections.push_back(san_francisco);
+	connections.push_back(las_vegas);
+	connections.push_back(salt_lake);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> LasVegasConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Las Vegas.
 {
-	std::tuple<std::string, int> Salt_Lake = std::make_tuple ("Salt Lake City", 421);
-	std::tuple<std::string, int> Reno = std::make_tuple ("Reno", 439);
-	std::tuple<std::string, int> San_Francisco = std::make_tuple ("San Francisco", 569);
-	connections.push_back(Salt_Lake);
-	connections.push_back(Reno);
-	connections.push_back(San_Francisco);
+	const std::tuple<std::string, int> salt_lake = std::make_tuple ("Salt Lake City", 421);
+	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 439);
+	const std::tuple<std::string, int> san_francisco = std::make_tuple ("San Francisco", 569);
+	connections.push_back(salt_lake);
+	connections.push_back(reno);
+	connections.push_back(san_francisco);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SanFranciscoConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from San Francisco.
 {
-	std::tuple<std::string, int> Reno = std::make_tuple ("Reno", 218);
-	std::tuple<std::string, int> Las_Vegas = std::make_tuple ("Las Vegas", 569);
-	std::tuple<std::string, int> Seattle = std::make_tuple ("Seattle", 808);
-	connections.push_back(Reno);
-	connections.push_back(Las_Vegas);
-	connections.push_back(Seattle);
+	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 218);
+	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 569);
+	const std::tuple<std::string, int> seattle = std::make_tuple ("Seattle", 808);
+	connections.push_back(reno);
+	connections.push_back(las_vegas);
+	connections.push_back(seattle);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SaltLakeCityConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Salt Lake City.
 {
-	std::tuple<std::string, int> Las_Vegas = std::make_tuple ("Las Vegas", 421);
-	std::tuple<std::string, int> Reno = std::make_tuple ("Reno", 518);
-	std::tuple<std::string, int> Seattle = std::make_tuple ("Seattle", 830);
-	connections.push_back(Las_Vegas);
-	connections.push_back(Reno);
-	connections.push_back(Seattle);
+	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 421);
+	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 518);
+	const std::tuple<std::string, int> seattle = std::make_tuple ("Seattle", 830);
+	connections.push_back(las_vegas);
+	connections.push_back(reno);
+	connections.push_back(seattle);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SeattleConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Seattle.
 {
-	std::tuple<std::string, int> Reno = std::make_tuple ("Reno", 704);
-	std::tuple<std::string, int> San_Francisco = std::make_tuple ("San Francisco", 808);
-	std::tuple<std::string, int> Salt_Lake = std::make_tuple ("Salt Lake City", 830);
-	connections.push_back(Reno);
-	connections.push_back(San_Francisco);
-	connections.push_back(Salt_Lake);
+	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 704);
+	const std::tuple<std::string, int> san_francisco = std::make_tuple ("San Francisco", 808);
+	const std::tuple<std::string, int> salt_lake = std::make_tuple ("Salt Lake City", 830);
+	connections.push_back(reno);
+	connections.push_back(san_francisco);
+	connections.push_back(salt_lake);
+	return connections;
+}
+
+inline std::vector<std::tuple<std::string, int>> GetCityConnection(const std::string&& city) {
+	std::vector<std::tuple<std::string, int>> connections;
+	if (city == "Reno")
+	{
+		return RenoConnections(connections);
+	}
+	else if (city == "Las Vegas")
+	{
+		return LasVegasConnections(connections);
+	}
+	else if (city == "San Francisco")
+	{
+		return SanFranciscoConnections(connections);
+	}
+	else if (city == "Salt Lake City")
+	{
+		return SaltLakeCityConnections(connections);
+	}
+	else if (city == "Seattle")
+	{
+		return SeattleConnections(connections);
+	}
+	std::cout << "GetCityConnection() Error: City not found." << std::endl;
 	return connections;
 }
