8174c525783876e1138e2c9cd01e014ff5a99f52
Ben Pearson <bpearson@nevada.unr.edu>
2020-04-27 16:20:01 -0700

    Cleaned up Driver.cpp

--- main/main/Driver.cpp
+++ main/main/Driver.cpp
@@ -4,14 +4,14 @@
 
 inline std::vector<std::tuple<std::string, int>> RenoConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Reno.
 {
-	std::tuple<std::string, int> san_francisco = std::make_tuple ("San Francisco", 218);
-	std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 439);
-	std::tuple<std::string, int> salt_lake = std::make_tuple ("Salt Lake City", 518);
-	std::tuple<std::string, int> seattle = std::make_tuple ("Seattle", 704);
-	connections.push_back(san_francisco);
-	connections.push_back(las_vegas);
-	connections.push_back(salt_lake);
-	connections.push_back(seattle);
+	const auto san_francisco = std::make_tuple ("San Francisco", 218);
+	const auto las_vegas = std::make_tuple ("Las Vegas", 439);
+	const auto salt_lake = std::make_tuple ("Salt Lake City", 518);
+	const auto seattle = std::make_tuple ("Seattle", 704);
+	connections.emplace_back(san_francisco);
+	connections.emplace_back(las_vegas);
+	connections.emplace_back(salt_lake);
+	connections.emplace_back(seattle);
 	return connections;
 }
 
@@ -21,44 +21,44 @@ inline std::vector<std::tuple<std::string, int>> LasVegasConnections(std::vector
 	auto reno = std::make_tuple ("Reno", 439);
 	auto san_francisco = std::make_tuple ("San Francisco", 569);
 	auto seattle = std::make_tuple ("Seattle", 1115);
-	connections.push_back(salt_lake);
-	connections.push_back(reno);
-	connections.push_back(san_francisco);
-	connections.push_back(seattle);
+	connections.emplace_back(salt_lake);
+	connections.emplace_back(reno);
+	connections.emplace_back(san_francisco);
+	connections.emplace_back(seattle);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SanFranciscoConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from San Francisco.
 {
-	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 218);
-	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 569);
-	const std::tuple<std::string, int> seattle = std::make_tuple ("Seattle", 808);
-	connections.push_back(reno);
-	connections.push_back(las_vegas);
-	connections.push_back(seattle);
+	const auto reno = std::make_tuple ("Reno", 218);
+	const auto las_vegas = std::make_tuple ("Las Vegas", 569);
+	const auto seattle = std::make_tuple ("Seattle", 808);
+	connections.emplace_back(reno);
+	connections.emplace_back(las_vegas);
+	connections.emplace_back(seattle);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SaltLakeCityConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Salt Lake City.
 {
-	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 421);
-	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 518);
-	const std::tuple<std::string, int> seattle = std::make_tuple ("Seattle", 830);
-	connections.push_back(las_vegas);
-	connections.push_back(reno);
-	connections.push_back(seattle);
+	const auto las_vegas = std::make_tuple ("Las Vegas", 421);
+	const auto reno = std::make_tuple ("Reno", 518);
+	const auto seattle = std::make_tuple ("Seattle", 830);
+	connections.emplace_back(las_vegas);
+	connections.emplace_back(reno);
+	connections.emplace_back(seattle);
 	return connections;
 }
 
 inline std::vector<std::tuple<std::string, int>> SeattleConnections(std::vector<std::tuple<std::string, int>>& connections) // Returns connections from Seattle.
 {
-	const std::tuple<std::string, int> reno = std::make_tuple ("Reno", 704);
-	const std::tuple<std::string, int> san_francisco = std::make_tuple ("San Francisco", 808);
-	const std::tuple<std::string, int> salt_lake = std::make_tuple ("Salt Lake City", 830);
-	const std::tuple<std::string, int> las_vegas = std::make_tuple ("Las Vegas", 1115);
-	connections.push_back(reno);
-	connections.push_back(san_francisco);
-	connections.push_back(salt_lake);
-	connections.push_back(las_vegas);
+	const auto reno = std::make_tuple ("Reno", 704);
+	const auto san_francisco = std::make_tuple ("San Francisco", 808);
+	const auto salt_lake = std::make_tuple ("Salt Lake City", 830);
+	const auto las_vegas = std::make_tuple ("Las Vegas", 1115);
+	connections.emplace_back(reno);
+	connections.emplace_back(san_francisco);
+	connections.emplace_back(salt_lake);
+	connections.emplace_back(las_vegas);
 	return connections;
 }
